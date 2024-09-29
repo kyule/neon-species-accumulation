d587cbcca050d2a6e3fb7da2014449e4142242e9
Kelsey Yule <kelsey.yule@gmail.com>
2024-09-28 17:52:34 -0700

    many updates, file structure change

--- code/202409/EstimateTimeToReachDiversity.R
+++ code/202409/EstimateTimeToReachDiversity.R
@@ -1,5 +1,66 @@
 ### Find sampling required to reach 90% diversity
 
+### This is the primary file for analyzing NEON species accumulation data
+
+#### BEFORE RUNNING
+# Users must define their own paths
+
+# datapath<-"user defined path"
+# codepath<-"user defined path"
+
+# Users need to indicate whether they want to get new set of results
+# These these steps are very time consuming so it is recommended that they are only done if necessary
+NewResults<-FALSE
+
+# And users should remove below line that loads in personal paths
+source("/Users/kelsey/Github/neon-species-accumulation/configini.R")
+
+#set seed
+set.seed(85705)
+
+# Load some necessary packages
+library("dplyr")
+library("ggplot2")
+library("MASS")
+
+# Load in the formatted clean data, or download and create it. 
+#Make sure the results are correctly configured
+
+if(NewResults==TRUE|file.exists(paste0(datapath,"results.Robj"))==FALSE){
+  source(paste0(codepath,"accumulation.R"))}else{load(file=paste0(datapath,"resultsFull.Robj"))}
+
+# Remove GUAN due to very low number of beetles captured. 
+### Only 36 beetles in 7 years of sampling, almost complete turnover in communities between most years
+results<- results[!names(results)=="GUAN"]
+
+### Pull Estimated Asymptotic and Observed Richness Values out of the results list -- full data only
+
+asyest.list<-lapply(results,function(item) data.frame(item$full$out$AsyEst))
+asyest <- bind_rows(asyest.list,.id="site")
+richness <- asyest[grep("Richness",rownames(asyest)),]
+
+#### Pull the turnovers out of the results list and input into the richness data frame
+
+turnover.list<-lapply(results,function(item) data.frame(item$turnover))
+turnover.list <- bind_rows(turnover.list,.id="site")
+turnover<-data.frame(turnover.list %>% 
+                       group_by(site) %>% 
+                       summarise(turnover=mean(total,na.rm=TRUE),years=length(year)))
+full.com <- left_join(richness,turnover,join_by("site"=="site"))
+
+full.com$propObs<-full.com$Observed/full.com$Estimator
+
+# Take into account the number of traps that have been sampled
+trap.list<-lapply(results,function(item) nrow(data.frame(item$full$traps)))
+trap.list <- melt(trap.list)
+names(trap.list)<-c("traps","site")
+full.com<-left_join(full.com,trap.list,join_by("site"=="site"))
+
+# Pull in dissimilarity measures
+dissim<-melt(lapply(results,function(item) mean(item$dissim)))
+names(dissim)<-c("dissim","site")
+full.com<-left_join(full.com,dissim,join_by("site"=="site"))
+
 # Pull Estimated and Observed Richness by sampling Values out of the results list -- full community only
 
 inext.list<-lapply(results,function(item) data.frame(item$full$out$iNextEst$size_based))
@@ -10,60 +71,50 @@ thresh90<-data.frame(site=full.com$site,thresh=full.com$Estimator*0.90)
 full.com$trapAvg<-full.com$traps/full.com$years
 thresh90<-left_join(thresh90,full.com[,c("site","trapAvg")],join_by("site"=="site"))
 
-# Find intersections
-find_intersections <- function(df,thresh,metric) {
-  site_data <- df[df$site == thresh$site, ]
+## Find where the threshold is crossed and scale years in inext data frame
+thresh90$t.thresh<-NA
+inext$y<-NA
+
+for (i in 1:nrow(thresh90)){
+  site_data<-inext[which(inext$site==thresh90$site[i]),]
+  inext$y[which(inext$site==thresh90$site[i])]<-inext$t[which(inext$site==thresh90$site[i])]/thresh90$trapAvg[i]
+  diff<-site_data$qD-thresh90$thresh[i]
   
-  # Loop through data and check for crossings
-  for (i in 1:(nrow(site_data) - 1)) {
-    if ((site_data$qD[i] < thresh$thresh && site_data$qD[i+1] > thresh$thresh) ||
-        (site_data$qD[i] > thresh$thresh && site_data$qD[i+1] < thresh$thresh)) {
-      
-      # Linear interpolation to find the exact crossing point
-      if (metric=="t"){
-        t1 <- site_data$t[i]
-        t2 <- site_data$t[i+1]
-        qD1 <- site_data$qD[i]
-        qD2 <- site_data$qD[i+1]
-        crossing_t <- t1 + (thresh$thresh - qD1) * (t2 - t1) / (qD2 - qD1)
-        
-        return(data.frame(site = thresh$site, t = crossing_t, qD = thresh$thresh))
-      }
-      
-      if (metric=="byYears")
-        y1 <- site_data$byYears[i]
-      y2 <- site_data$byYears[i+1]
-      qD1 <- site_data$qD[i]
-      qD2 <- site_data$qD[i+1]
-      crossing_t <- y1 + (thresh$thresh - qD1) * (y2 - y1) / (qD2 - qD1)
-      
-      return(data.frame(site = thresh$site, t = crossing_t, qD = thresh$thresh))
-    }
+  for (j in 1:length(diff)){
+    if (diff[j]>0){break}
   }
-  return(NULL)
+  
+  v<-site_data[c(j-1,j),c("t","qD")]
+  thresh90$t.thresh[i]<-v$t[1] + (thresh90$thresh[i] - v$qD[1]) * (v$t[2] - v$t[1]) / (v$qD[2] - v$qD[1])
+  
 }
 
-# Get all intersections 
-intersections <- do.call(rbind, lapply(1:nrow(thresh90), function(i) {
-  find_intersections(inext, thresh90[i, ],"t")
-}))
+
+# Calculate the thresholds based on year instead 
+thresh90$y.thresh<-thresh90$t.thresh/thresh90$trapAvg
+full.com<-left_join(full.com,thresh90,join_by("site"=="site"))
+
+# Bring in field site data
+
+sites<-read.csv("/Users/kelsey/Github/neon-species-accumulation/data/NEON_Field_Site_Metadata_20240926.csv")
 
 
 
 
-ggplot(inext, aes(x = t, y = qD, color=site , group=site)) +
+
+
+#Plot the accumulation curves and estimated number of years
+ggplot(inext, aes(x = y, y = qD, color=site , group=site)) +
   geom_line() +
-  labs(x = "Number of Traps", y = "Estimated Richness") +
+  labs(x = "Years", y = "Estimated Richness") +
   theme_minimal() +
   scale_color_brewer(palette = "Set1") +
   geom_hline(data = thresh90, aes(yintercept = thresh, color = site), linetype = "dashed") +
-  geom_point(data = intersections, aes(x = t, y = qD), color = "darkgrey", size = 2) 
+  geom_point(data = thresh90, aes(x = y.thresh, y = thresh), color = "darkgrey", size = 2) 
 
-#Calculate the number of years based on recent per year sampling effort
 
-thresh90<-left_join(thresh90,intersections[c("site","t")],join_by("site"=="site"))
-thresh90$yearsReq<-thresh90$t/thresh90$trapno
-thresh90
+
+
 
 # Scale the X axis by years of sampling
 
